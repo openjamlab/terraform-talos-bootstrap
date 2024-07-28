18261c51b132659403fc39a4404dc8959fa66c85
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2024-07-28 13:47:47 +0100

    feat: Add variable descriptions and defaults

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "cluster" {
+  description = "Talos cluster configuration"
   type = object({
     name               = string
     vip_address        = string
@@ -8,9 +9,19 @@ variable "cluster" {
     pod_cidr           = string
     service_cidr       = string
   })
+  default = {
+    name               = "myCluster"
+    vip_address        = "10.0.0.30"
+    kubernetes_version = "v1.30.2"
+    talos_version      = "v1.7.5"
+    cni                = "flannel"
+    pod_cidr           = "10.244.0.0/16"
+    service_cidr       = "10.96.0.0/12"
+  }
 }
 
 variable "node_data" {
+  description = "Talos Node configuration"
   type = object({
     default_gateway = string
     dns_endpoint    = string
@@ -28,16 +39,55 @@ variable "node_data" {
       }))
     })
   })
+  default = {
+    default_gateway = "10.0.0.1"
+    dns_endpoint    = "1.1.1.1"
+    ntp_endpoint    = "time.cloudflare.com"
+
+    control_plane = {
+      nodes = {
+        "10.0.0.31" = {
+          install_disk = "/dev/sda"
+        },
+        "10.0.0.32" = {
+          install_disk = "/dev/sda"
+        },
+        "10.0.0.33" = {
+          install_disk = "/dev/sda"
+        }
+      }
+    }
+
+    worker = {
+      nodes = {
+        "10.0.0.34" = {
+          install_disk = "/dev/sda"
+        },
+        "10.0.0.35" = {
+          install_disk = "/dev/sda"
+        },
+        "10.0.0.36" = {
+          install_disk = "/dev/sda"
+        }
+      }
+    }
+  }
 }
 
 variable "extra_global_config" {
-  type = string
+  description = "Extra global configuration"
+  type        = string
+  default     = ""
 }
 
 variable "extra_controlplane_config" {
-  type = string
+  description = "Extra control plane configuration"
+  type        = string
+  default     = ""
 }
 
 variable "extra_worker_config" {
-  type = string
+  description = "Extra worker configuration"
+  type        = string
+  default     = ""
 }
